7b41c6d328e54e57f76da5f63e2a215a428f172d
Ueda-Masashi <mumunohanata@gmail.com>
2020-05-29 10:31:38 +0900

    カスタムクラスができない

---
+++ table test/table test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  table test
+//
+//  Created by むむ on 2020/05/29.
+//  Copyright © 2020 DEAUX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
